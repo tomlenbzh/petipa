62dc6c5f7888883476fca97868c4670d6b9eefa9
tomlenbzh <thomas.lenoel@epitech.eu>
2021-01-30 11:40:40 +0100

    Petipa - [FIRST COMMIT]

---
+++ js/petipa.js
@@ -0,0 +1,68 @@
+const topSection = document.getElementById('top');
+const projectSection = document.getElementById('project');
+const aboutMeSection = document.getElementById('aboutMe');
+const ourFeesSection = document.getElementById('ourFees');
+const sourcesSection = document.getElementById('sources');
+const testimoniesSection = document.getElementById('testimonies');
+const contactMeSection = document.getElementById('contactMe');
+const dropdownContainer = document.getElementById('dropdown-container');
+const headerOffset = 150;
+
+dropdownContainer.style.display = 'none';
+
+function navigateToTop() {
+  zenscroll.setup(500, 0);
+  zenscroll.to(topSection);
+}
+
+function navigateToProject(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(projectSection);
+  if (closeMenu === true) {
+    toggleMenu();
+  }
+}
+
+function navigateToAboutMe(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(aboutMeSection);
+  if (closeMenu) {
+    toggleMenu();
+  }
+}
+
+function navigateToOurFees(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(ourFeesSection);
+  if (closeMenu) {
+    toggleMenu();
+  }
+}
+
+function navigateToSources(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(sourcesSection);
+  if (closeMenu) {
+    toggleMenu();
+  }
+}
+
+function navigateToTestimonies(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(testimoniesSection);
+  if (closeMenu) {
+    toggleMenu();
+  }
+}
+
+function navigateToContactMe(closeMenu) {
+  zenscroll.setup(500, 150);
+  zenscroll.to(contactMeSection);
+  if (closeMenu) {
+    toggleMenu();
+  }
+}
+
+function toggleMenu() {
+  dropdownContainer.style.display = dropdownContainer.style.display === 'block' ? 'none' : 'block';
+}
